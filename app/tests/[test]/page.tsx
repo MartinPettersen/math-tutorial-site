2281304d95d96e89139b7832f336dc07be9a51df
MartinPettersen <searchingtheskyfor@gmail.com>
2023-12-22 11:55:06 +0100

    imrpove style

--- app/tests/[test]/page.tsx
+++ app/tests/[test]/page.tsx
@@ -29,8 +29,12 @@ async function Subject({ params }: Props) {
     <div className="h-[90%] flex items-center justify-center">
       <div className="flex flex-col gap-4 items-center justify-center w-[80%] h-[100%]">
         <header className="font-3xl  font-extrabold">{subject.subject}</header>
+        <div>Drag and drop the operator and number needed to fullfill the task</div>
         <div className="flex flex-col sm:flex-row gap-2 sm:gap-4  p-2 rounded-md">
           <Container subject="Division"/>
+        </div>
+        <div className="border border-black p-4 bg-emerald-500 hover:bg-emerald-300 rounded-md">
+          Check Answer
         </div>
       </div>
     </div>
--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Logo() {
   return (
     <Link href="/" className="flex flex-col items-center sm:gap-0 gap-2 p-2">
-      <div className="text-4xl font-bold">+Math-</div>
+      <div className="text-4xl flex font-bold"><div className="text-orange-400">+</div>Math<div className="text-sky-400">-</div></div>
       <div>Refresh your math knowledge</div>
     </Link>
   );
